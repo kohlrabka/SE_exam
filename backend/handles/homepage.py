5b566389c7befddc6d9c82b9b051e28652b2c0d4
kohlrabka <DmitriyVlasovDV1@yandex.ru>
2024-10-28 13:44:32 +0300

    Added button

--- backend/handles/homepage.py
+++ backend/handles/homepage.py
@@ -16,3 +16,11 @@ def homepage(request: Request):
 def homepage_create_lobby(request: Request):
     print("clicked")
     return "459hvdf5n9"
+
+@router.get('/homepage/join_lobby', response_class=HTMLResponse)
+def homepage_join_lobby(code):
+    print(f"clicked 2 {code}")
+    return JSONResponse(content={
+        'is_code_valid': False
+
+    })
