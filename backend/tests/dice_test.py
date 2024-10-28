e0aa0c68c36db9cd9754c7faf8d461225b457789
Святослав Кураленок <skuralenok@MacBook-Air-2.local>
2024-10-28 14:25:18 +0300

    Add: score function

--- backend/tests/dice_test.py
+++ backend/tests/dice_test.py
@@ -18,7 +18,50 @@ def test_dices_random():
         assert abs(number_probability[key] / (n * 6) - 1 / 6) < 0.03
 
 def test_dices_score():
-    n = 100000
-    for _ in range(0, n):
-        tmp = dice.get_dices()
-        assert dice.get_scrore(tmp) == 1
+    dices = [1, 2, 3, 4, 6]
+    assert dice.get_score(dices) == 5
+
+    dices = [1, 1, 2, 3, 4]
+    assert dice.get_score(dices) == 6
+
+    dices = [1, 2, 2, 3, 4]
+    assert dice.get_score(dices) == 6
+
+    dices = [1, 2, 4, 6, 6]
+    assert dice.get_score(dices) == 6
+
+    dices = [1, 5, 4, 5, 6]
+    assert dice.get_score(dices) == 6
+
+    dices = [4, 5, 4, 5, 6]
+    assert dice.get_score(dices) == 6 * 6
+
+    dices = [4, 3, 4, 6, 6]
+    assert dice.get_score(dices) == 6 * 6
+
+    dices = [1, 1, 1, 3, 4]
+    assert dice.get_score(dices) == 6 * 6 * 6
+
+    dices = [1, 1, 5, 3, 1]
+    assert dice.get_score(dices) == 6 * 6 * 6
+
+    dices = [1, 4, 5, 4, 4]
+    assert dice.get_score(dices) == 6 * 6 * 6
+
+    dices = [1, 2, 3, 4, 5]
+    assert dice.get_score(dices) == 6 * 6 * 6 * 6
+
+    dices = [2, 3, 4, 5, 6]
+    assert dice.get_score(dices) == 6 * 6 * 6 * 6
+
+    dices = [1, 4, 1, 4, 4]
+    assert dice.get_score(dices) == 6 * 6 * 6 * 6 * 6
+
+    dices = [2, 5, 2, 5, 5]
+    assert dice.get_score(dices) == 6 * 6 * 6 * 6 * 6
+
+    dices = [2, 5, 5, 5, 5]
+    assert dice.get_score(dices) == 6 * 6 * 6 * 6 * 6 * 6
+
+    dices = [6, 6, 6, 6, 6]
+    assert dice.get_score(dices) == 6 * 6 * 6 * 6 * 6 * 6 * 6
